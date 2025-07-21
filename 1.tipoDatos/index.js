66b497f5449fc20868140633df7714a22bbe8a90
peryz30 <percynoeyarihuaman@gmail.com>
2025-07-21 09:52:03 -0500

    feat:añade estructura de tipo de datos
    y dando ejemplos d como funciona

---
+++ 1.tipoDatos/index.js
@@ -0,0 +1,23 @@
+console.log("mensaje");
+
+suma=4+4
+console.log(suma)
+
+7 // Dato numerico entero positivo
+-3 // Dato numerico entero egativo
+9.0 // Dato numerico decimal positivo
+-6.0 // Dato numerico decimal negativo
+.6 // Dato numerico decimal positivo
+NaN // Dato numerico not number
+console.log(typeof(NaN))
+console.log(typeof(10))
+
+// "a" // Datp tipo texto caracter
+// 'a' // Dato tipo texto de un caracter
+// `a` // Dato tipo texto de un caracter 
+console.log(typeof("f"))
+console.log(typeof("@"))
+console.log(typeof("hola como estas"))
+
+console.log(typeof(false))
+console.log(typeof(true))
