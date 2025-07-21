ac6711f83b23e9f580637a8fd0d43f7fca81ea13
peryz30 <percynoeyarihuaman@gmail.com>
2025-07-21 09:53:42 -0500

    feat: añade la estructura de variables
     dando ejemlos de como declarar una variable

---
+++ 2.variables/index.js
@@ -0,0 +1,16 @@
+// Declarar la variable usando la palabra reservada y asignandole un nombre
+let nombreAlumno
+// Inicializacion de la variable en este paso es cuando se le asignara un tpo de dato a la variable
+// llamo a la varable, hago uso del operador de asignacion, le doy el dato
+nombreAlumno="percy"
+nombreAlumno="erick"
+console.log(nombreAlumno)
+
+// Existe una tecnica donde declaramos e inicializamos una variable en una sola linia de codigo
+let edadAlumno=50
+edadAlumno=30
+console.log(edadAlumno)
+// solo se usara let cuando creamos la variable luego si deseamos modificar el valor o llamr a la variable solo usaremos el nombre
+
+const PI=3.141516
+console.log(PI)
