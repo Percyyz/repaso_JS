d5bf916c0da5f0f1417191ccd13ff5b8a79e2b8b
peryz30 <percynoeyarihuaman@gmail.com>
2025-07-23 17:09:24 -0500

    feat: añade ejemplos claros de operadores

---
+++ 3.operadores/index.js
@@ -0,0 +1,50 @@
+// operador unario
+// operador de negacion
+// operador esta a la izquierda del valor y como resultado obtenemos otro valor
+
+let negacion=!false
+//expresion es un valor complejo que sera solucionado y nos producira o retornara un nuevo valor
+console.log(negacion)
+
+// Operadore binarios son los que evaluan dos valores y nos dan como resultado un nuevo valor
+let suma=34+20
+let resta=6-2
+let producto=4*5
+let divicion=89/4
+let potencia=3**2
+let residuo=10%3
+
+// operadoes de comparacion 
+let igual = 5 == 5
+let estrictamenteIgual = 5 === 5
+let diferente = 10 != 5
+let estrictamenteDiferente = 10 !== "10"
+let mayorQue = 15 > 10
+let menorQue = 8 < 12
+let mayorIgual = 20 >= 15
+let menorIgual = 7 <= 10
+
+// operadores logicos
+let AND = true && false    // &&-->AND
+let OR = true || false    // ||-->OR
+let NOT = !true           // !--->NOT
+
+// operadores ternarios
+let respuesta=5<7?"esta bien mascota":"como va ser mayor animal"
+console.log(respuesta)
+
+let numero=7 % 2 === 0?"es par":"es impar"
+console.log(numero)
+
+let edad=50
+let result=edad>18?"es mayor":"es menor"
+console.log(result)
+
+
+
+console.log(suma)
+console.log(resta)
+console.log(producto)
+console.log(divicion)
+console.log(potencia)
+console.log(residuo)
