c7897d5ae3d7fae9558bb007583ddb8bb2c29b56
peryz30 <percynoeyarihuaman@gmail.com>
2025-09-15 10:37:59 -0500

    feat: Añade un nuevo archivo llamada declaracion
     Para dar ejemplos de como funciona

---
+++ 5.funciones/Declaracion.js
@@ -0,0 +1,14 @@
+// crear funciones para el manejo de operadores aritmeticos
+
+let numeroUno=34
+let numeroDos=45
+
+function suma(a, b) {
+    return a+b
+ }
+
+ // llamada de funcion con argumentos posicionales
+console.log(suma(34, 45))
+
+// llamada de funcion con argumentos nominales
+console.log(suma(a=34, b=45))
